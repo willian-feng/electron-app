b0ebcbb5ff91f15fa9f4a1f95c79c1a152d09d55
fengweiliang <0712024015@163.com>
2019-08-12 18:39:05 +0800

    modify options

--- index.js
+++ index.js
@@ -1,56 +1,107 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require('electron-updater');
+const path = require('path');
+
+const feedUrl = `http://127.0.0.1:5500/win32`; // 更新包位置
 
 // 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
 // 垃圾回收的时候，window对象将会自动的关闭
-let win
+let win, webContents;
 
-function createWindow () {
+function createWindow() {
   // 创建浏览器窗口。
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+	width: 1024,
+    height: 768,
+    //禁用跨域检查
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      webSecurity: false,
+      // preload: path.join(__dirname, 'preload.js')
     }
-  })
+  });
 
   // 加载index.html文件
-  win.loadFile('index.html')
+  win.loadFile('./index.html');
 
   // 打开开发者工具
-  win.webContents.openDevTools()
+  win.webContents.openDevTools();
 
   // 当 window 被关闭，这个事件会被触发。
   win.on('closed', () => {
     // 取消引用 window 对象，如果你的应用支持多窗口的话，
     // 通常会把多个 window 对象存放在一个数组里面，
     // 与此同时，你应该删除相应的元素。
-    win = null
-  })
+    win = null;
+  });
 }
+
+// 主进程主动发送消息给渲染进程函数
+function sendUpdateMessage(message, data) {
+  console.log({ message, data });
+  webContents.send('message', { message, data });
+}
+
+// 主进程监听渲染进程传来的信息
+ipcMain.on('update', (e, arg) => {
+  console.log("update");
+  checkForUpdates();
+});
+
+let checkForUpdates = () => {
+  // 配置安装包远端服务器
+  autoUpdater.setFeedURL(feedUrl);
+
+  // 下面是自动更新的整个生命周期所发生的事件
+  autoUpdater.on('error', function (message) {
+    sendUpdateMessage('error', message);
+  });
+  autoUpdater.on('checking-for-update', function (message) {
+    sendUpdateMessage('checking-for-update', message);
+  });
+  autoUpdater.on('update-available', function (message) {
+    sendUpdateMessage('update-available', message);
+  });
+  autoUpdater.on('update-not-available', function (message) {
+    sendUpdateMessage('update-not-available', message);
+  });
+
+  // 更新下载进度事件
+  autoUpdater.on('download-progress', function (progressObj) {
+    sendUpdateMessage('downloadProgress', progressObj);
+  });
+  // 更新下载完成事件
+  autoUpdater.on('update-downloaded', function (event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
+    sendUpdateMessage('isUpdateNow');
+    ipcMain.on('updateNow', (e, arg) => {
+      autoUpdater.quitAndInstall();
+    });
+  });
+
+  //执行自动更新检查
+  autoUpdater.checkForUpdates();
+};
 
 // Electron 会在初始化后并准备
 // 创建浏览器窗口时，调用这个函数。
 // 部分 API 在 ready 事件触发后才能使用。
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // 当全部窗口关闭时退出。
 app.on('window-all-closed', () => {
   // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
   // 否则绝大部分应用及其菜单栏会保持激活。
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // 在macOS上，当单击dock图标并且没有其他窗口打开时，
   // 通常在应用程序中重新创建一个窗口。
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // 在这个文件中，你可以续写应用剩下主进程代码。
 // 也可以拆分成几个文件，然后用 require 导入。
