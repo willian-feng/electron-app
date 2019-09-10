323cd19618e03bed271124b56a9e45de7d3dcce7
fengweiliang <0712024015@163.com>
2019-09-10 19:54:14 +0800

    modify codetranslate page

--- src/routes/TransCode/components/index.js
+++ src/routes/TransCode/components/index.js
@@ -1,17 +1,71 @@
 import React from 'react';
-import { connect } from 'dva';
-import cc from './index.module.less'
+import { Row, Col, Select, Input } from 'antd';
+import { SELECT_LIST } from '../constants';
+import cc from './index.module.less';
 
-@connect(state => ({
-  ...state.code
-}))
+const keys = SELECT_LIST.map(s => ({ [s.value]: s.value}));
+
 export default class App extends React.Component {
-  state = {}
+  state = {
+    leftSelect: '',
+    rightSelect: '',
+    leftValue: '',
+    rightValue: ''
+  }
+
+  transformValue = value => {
+    switch (this.state.rightSelect) {
+      case keys['bin']:
+        return '';
+      default:
+        return value;
+    }
+  }
+
+  inputChange = value => {
+    this.setState(() => {
+      const rightValue = this.transformValue(value);
+      return { leftValue: value, rightValue };
+    });
+  }
+
+  selectChange = (value, isRight) => {
+    this.setState(() => {
+      return isRight ? { rightSelect: value } : { leftSelect: value };
+    });
+  }
+
+  selectDiv = (isRight) => {
+    const disabled = SELECT_LIST.find(select => select.value === this.state.leftSelect);
+    return (
+      <Select onChange={value => this.selectChange(value, isRight)}>
+        {SELECT_LIST.map(li => (
+          <Select.Option key={li.value} value={li.value} disabled={isRight ? disabled : false}>
+            {li.title}
+          </Select.Option>
+        ))}
+      </Select>
+    );
+  }
 
   render() {
     return (
       <div className={cc.root}>
-        123 456
+        <Row gutter={10}>
+          <Col span={12}>
+            {this.selectDiv(false)}
+            <Input.TextArea
+              autosize={{ minRows: 15, maxRows: 15 }}
+              value={this.state.leftValue}
+              onChange={this.inputChange} />
+          </Col>
+          <Col span={12}>
+            {this.selectDiv(true)}
+            <Input.TextArea
+              autosize={{ minRows: 15, maxRows: 15 }}
+              value={this.state.rightValue} />
+          </Col>
+        </Row>
       </div>
     );
   }
---
+++ src/routes/TransCode/constants.js
@@ -0,0 +1,6 @@
+export const SELECT_LIST = [
+  { title: '二进制', value: 'bin' },
+  { title: '十六进制', value: 'Hex' },
+  { title: 'ASCII', value: 'ascII' },
+  { title: 'UTF-8', value: 'utf8' }
+];
